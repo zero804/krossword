af097b5eef06797d9019c902d481eed26d094dc6
Cilium <Cilium@server>
2014-07-18 10:38:03 +0200

    Last commit was a bit wrong...

--- src/cells/cluecell.cpp
+++ src/cells/cluecell.cpp
@@ -1152,16 +1152,11 @@ void ClueCell::drawClueNumber(QPainter *p, const QStyleOptionGraphicsItem *optio
         QFontMetrics fontMetrics(font);
         QRect rect = fontMetrics.boundingRect(text);
         rect.setWidth(fontMetrics.width(text));
-<<<<<<< HEAD
         QRect trimmedRect = KrosswordTheme::trimmedRect(option->rect, krossWord()->theme()->marginsClueCell(levelOfDetail));
 
         p->drawText(KrosswordTheme::rectAtPos(trimmedRect, rect, krossWord()->theme()->clueNumberPos()), text);
 //  QPoint topLeft = option->rect.bottomRight() - QPoint( rect.width(), rect.height() );
 //  p->drawText( QRect( topLeft, rect.size()).adjusted(-3, -3, -3, -3), text );
-=======
-        QRect trimmedRect = KrosswordTheme::trimmedRect(option->rect, krossWord()->theme()->marginsClueCell(levelOfDetail));
-        p->drawText(KrosswordTheme::rectAtPos(trimmedRect, rect, krossWord()->theme()->clueNumberPos()), text);
->>>>>>> c9d6d44c2f6f3c471eded0cfc56f0bc4e564160d
     }
 }
 
