d695711890f05f35aa24d1fe290dda3e03d8e9b6
Cilium <Cilium@server>
2014-12-24 13:42:42 +0100

    Re-enabled dictionary: it's still very buggy, but some functions work.

--- src/krosswordpuzzle.cpp
+++ src/krosswordpuzzle.cpp
@@ -418,10 +418,9 @@ void KrossWordPuzzle::currentTabChanged(int index)
         crosswordGameList << separator2;
         crosswordGameList << m_mainCrossword->action("game_close");
 
-        /* FIXME: Dictionary is temporary inactive
         optionsList << m_mainCrossword->action(m_mainCrossword->actionName(CrossWordXmlGuiWindow::Options_Dictionaries));
         optionsList << separator;
-        */
+
         optionsList << m_mainCrossword->toolBarMenuAction();
         optionsList << m_mainCrossword->action(m_mainCrossword->actionName(CrossWordXmlGuiWindow::ShowClueDock));
         optionsList << m_mainCrossword->action(m_mainCrossword->actionName(CrossWordXmlGuiWindow::ShowUndoViewDock));
@@ -439,10 +438,9 @@ void KrossWordPuzzle::currentTabChanged(int index)
         libraryGameList << m_mainLibrary->action("library_import");
         libraryGameList << m_mainLibrary->action("library_export");
 
-        /* FIXME: Dictionary is temporary inactive
         optionsList << m_mainCrossword->action(m_mainCrossword->actionName(CrossWordXmlGuiWindow::Options_Dictionaries));
         optionsList << separator;
-        */
+
         optionsList << m_mainLibrary->toolBarMenuAction();
     }
 
