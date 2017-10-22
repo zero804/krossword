9d90ccb28cad46e43418e8e78db11b07dbf8ba03
Caig <caig@server>
2017-10-22 16:21:51 +0200

    updated translations

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
-$XGETTEXT *.cpp dialogs/*.cpp cells/*.cpp io/*.cpp -o $podir/krosswordpuzzle.pot
+$XGETTEXT *.cpp dialogs/*.cpp cells/*.cpp io/*.cpp library/*.cpp -o $podir/krossword.pot
